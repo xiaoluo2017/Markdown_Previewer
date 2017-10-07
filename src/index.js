4ce0f034793355215fde075aa4473e7dc3b47544
Xiao Luo <xiao2017luo@gmail.com>
2017-10-07 12:12:55 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Textarea from './Textarea';
+import './index.css';
+
+ReactDOM.render(
+  <Textarea />,
+  document.getElementById('content')
+); 
